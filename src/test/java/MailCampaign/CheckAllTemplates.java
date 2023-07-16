045c91c5e5dda270041f4e7f49ed9566b85b16d9
Mohanad Abolela <mohanad_1612@yahoo.com>
2023-07-16 16:55:24 +0300

    add all templates

--- src/test/java/MailCampaign/CheckAllTemplates.java
+++ src/test/java/MailCampaign/CheckAllTemplates.java
@@ -38,13 +38,142 @@ public class CheckAllTemplates {
     }
 
     @Test(dependsOnMethods = "homePage")
-    public void selectOneTimeCampaign() throws InterruptedException {
+    public void selectBackToSchoolTemplate() throws InterruptedException {
         SeleniumActions actions = new SeleniumActions();
         Locators locators = new Locators();
         CreateCampaigns oneTime = new CreateCampaigns();
         oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
         Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
         actions.click(locators.backButton);
+    }
+
+    @Test(dependsOnMethods = "selectBackToSchoolTemplate")
+    public void selectBackInStockTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectBackInStockTemplate")
+    public void selectDiscountOnSelectedItemsTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectDiscountOnSelectedItemsTemplate")
+    public void selectShoppingSeasonSummerSaleTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectShoppingSeasonSummerSaleTemplate")
+    public void select24HrsFlashSaleTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "select24HrsFlashSaleTemplate")
+    public void select24HrsFlashSale2Template() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "select24HrsFlashSale2Template")
+    public void selectLaborDayTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectLaborDayTemplate")
+    public void selectEbookReleasedTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+    @Test(dependsOnMethods = "selectEbookReleasedTemplate")
+    public void selectSummerSaleTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectSummerSaleTemplate")
+    public void selectWomenDayTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+    @Test(dependsOnMethods = "selectWomenDayTemplate")
+    public void selectHelloBlackFridayTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+
+    @Test(dependsOnMethods = "selectHelloBlackFridayTemplate")
+    public void selectDiscountEmailTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
+    }
+    @Test(dependsOnMethods = "selectDiscountEmailTemplate")
+    public void selectWelcomeEmailTemplate() throws InterruptedException {
+        SeleniumActions actions = new SeleniumActions();
+        Locators locators = new Locators();
+        CreateCampaigns oneTime = new CreateCampaigns();
+        oneTime.oneTimeCampaign(locators.hoverOnBackToSchool, locators.selectBackToSchool);
+        Assert.assertEquals(actions.getText(locators.assertBackToSchool), "Shopping Season Back To School");
+        actions.click(locators.backButton);
+
     }
 
 }
